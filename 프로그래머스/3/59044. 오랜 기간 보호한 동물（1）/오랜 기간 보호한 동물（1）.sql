cf17baf081b70938c626649d514ea449c0cbc03a
Jin <bblinging22@gmail.com>
2025-04-30 09:28:38 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,14 +1,12 @@
--- 입양을 못 간 동물 --> OUTS에 없는 --
--- 가장 오래 보호소 : 들어온 날짜가 오래된 --
--- 3마리 -> LIMIT --
--- 이름, 보호시작일 조회 --
+-- 이름, 보호 시작일 조회 --
 SELECT INS.NAME AS NAME, INS.DATETIME AS DATETIME
 FROM ANIMAL_INS AS INS
 -- 조인 시작 --
 LEFT JOIN ANIMAL_OUTS AS OUTS
-    ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
--- OUTS에 없는 --
-WHERE OUTS.ANIMAL_ID IS NULL
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+-- 아직 입양을 못간(조건) --
+WHERE OUTS.DATETIME IS NULL
 -- 보호 시작일 순 정렬 --
-ORDER BY INS.DATETIME
-LIMIT 3;
+ORDER BY DATETIME 
+-- 3마리 리미트 --
+LIMIT 3
